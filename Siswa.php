008d3f44025fd5330bfa4bcbdb7569804be2312c
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-14 11:33:54 +0700

    Update Siswa.php

--- Siswa.php
+++ Siswa.php
@@ -6,8 +6,14 @@ class Siswa {
     protected $email;
     protected $nohp;
     protected $kelas = array(); // Array Of Object Kelas
+    protected $guru = array();
+    protected $jadwal = array();
+    protected $mapel = array();
 
-   
+    /**prote
+     * Set Nama
+     * @param String $nama Set Nama Siswa
+     */
     public function setNama($nama) 
     {
         $this->nama = $nama;
@@ -33,6 +39,21 @@ class Siswa {
         array_push($this->kelas, $kelas);
     }
 
+    public function addGuru($guru) 
+    {
+        array_push($this->guru, $guru);
+    }
+
+     public function addJadwal($jadwal) 
+    {
+        array_push($this->jadwal, $jadwal);
+    }
+
+     public function addMapel($mapel) 
+    {
+        array_push($this->mapel, $mapel);
+    }
+
     public function getNama() 
     {
         return $this->nama;
@@ -56,5 +77,18 @@ class Siswa {
     public function getKelas() 
     {
         return $this->kelas;
+    }
+
+    public function getGuru() 
+    {
+        return $this->guru;
+    }
+    public function getJadwal() 
+    {
+        return $this->jadwal;
+    }
+    public function getMapel() 
+    {
+        return $this->mapel;
     }
 }
